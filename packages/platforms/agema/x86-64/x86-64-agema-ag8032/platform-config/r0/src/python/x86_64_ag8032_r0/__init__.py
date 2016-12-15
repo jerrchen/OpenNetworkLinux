0a8615316eaf0639326df53d7bbac0d0d8cca0f3
root <root@ip-172-31-10-136.us-west-2.compute.internal>
2016-12-15 23:41:43 +0000

    8032 prototype

---
+++ packages/platforms/agema/x86-64/x86-64-agema-ag8032/platform-config/r0/src/python/x86_64_ag8032_r0/__init__.py
@@ -0,0 +1,8 @@
+from onl.platform.base import *
+from onl.platform.agema import *
+
+class OnlPlatform_x86_64_ag8032_r0(OnlPlatformAgema,
+                                               OnlPlatformPortConfig_48x10_6x40):
+    MODEL="ag8032"
+    PLATFORM="x86-64-ag8032-r0"
+    SYS_OBJECT_ID=".8032.1"
